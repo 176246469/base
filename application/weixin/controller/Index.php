71e63ea869d81b79d75d65b66a76f97cc2b67c31
176246469 <176246469@qq.com>
2017-08-16 13:28:54 +0800

    6666

--- application/weixin/controller/Index.php
+++ application/weixin/controller/Index.php
@@ -4,13 +4,14 @@ use think\Request;
 use think\Session;
 
 
-
+use app\admin\weixin\wechatCallbackapiTest;
 class Index extends  BaseController
 {
 
         public function index(Request $request){
 
-echo '123';
+            $wechatObj = new wechatCallbackapiTest();
+            $wechatObj->valid();
         }
 
 }
