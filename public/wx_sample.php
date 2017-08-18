4798ac5b7729849f9b40f10dc7827adea2403b03
176246469 <176246469@qq.com>
2017-08-18 15:04:23 +0800

    66
    66

--- public/wx_sample.php
+++ public/wx_sample.php
@@ -19,12 +19,20 @@
             file_put_contents('weixin_log.txt', "IP=".$_SERVER['REMOTE_ADDR'].PHP_EOL,FILE_APPEND); //记录访问IP到log日志
             file_put_contents('weixin_log.txt', "QUERY_STRING=".$_SERVER['QUERY_STRING'].PHP_EOL,FILE_APPEND);//记录请求字符串到log日志
             file_put_contents('weixin_log.txt', '$_GET[echostr])='.htmlspecialchars($_GET['echostr']).PHP_EOL,FILE_APPEND); //记录是否获取到echostr参数
-         $postStr = file_get_contents("php://input");
+            $postStr = file_get_contents("php://input");
            if(!empty($postStr)){
-         
-        file_put_contents('weixin_log.txt', "postStr=".$postStr.PHP_EOL,FILE_APPEND );                 
+                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[%s]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>";     
+                $msgType = "text";
+                $contentStr = "Welcome to wechat world!";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                    exit($resultStr);               
            }
-    exit(htmlspecialchars($_GET['echostr']));  
+
                
                       /*
                       //记录访问IP到log日志
