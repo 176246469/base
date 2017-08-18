1d989861a284aaedf442e15f86a325638b54a7ae
176246469 <176246469@qq.com>
2017-08-18 14:33:33 +0800

    8

--- public/wx_sample.php
+++ public/wx_sample.php
@@ -16,7 +16,10 @@
                 $tmpStr = sha1( $tmpStr );
                 //验证成功
                 if( $tmpStr == $signature ){ 
-exit(666);
+            file_put_contents('weixin_log.txt', "IP=".$_SERVER['REMOTE_ADDR'].PHP_EOL,FILE_APPEND); //记录访问IP到log日志
+            file_put_contents('weixin_log.txt', "QUERY_STRING=".$_SERVER['QUERY_STRING'].PHP_EOL,FILE_APPEND);//记录请求字符串到log日志
+            file_put_contents('weixin_log.txt', '$_GET[echostr])='.htmlspecialchars($_GET['echostr']).PHP_EOL,FILE_APPEND); //记录是否获取到echostr参数
+            exit(htmlspecialchars($_GET['echostr']));    
                 /*
                 if (!empty($postStr)){
                 $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
@@ -29,6 +32,10 @@ exit(666);
                 $contentStr = "Welcome to wechat world!";
                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
                     exit($resultStr);
-                } */  }
+                } */  }else{
+
+                         file_put_contents('weixin_log.txt', "tmpStr=".$tmpStr ); //记录访问IP到log日志  
+                           file_put_contents('weixin_log.txt', "signature=".$signature ); //记录访问IP到log日志  
+                }
 
 ?>
