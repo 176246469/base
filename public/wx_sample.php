0ef464280407091841bd85dc21118cbe7301563b
176246469 <176246469@qq.com>
2017-08-18 15:40:28 +0800

    888

--- public/wx_sample.php
+++ public/wx_sample.php
@@ -19,7 +19,7 @@
             file_put_contents('weixin_log.txt', "IP=".$_SERVER['REMOTE_ADDR'].PHP_EOL,FILE_APPEND); //记录访问IP到log日志
             file_put_contents('weixin_log.txt', "QUERY_STRING=".$_SERVER['QUERY_STRING'].PHP_EOL,FILE_APPEND);//记录请求字符串到log日志
             file_put_contents('weixin_log.txt', '$_GET[echostr])='.htmlspecialchars($_GET['echostr']).PHP_EOL,FILE_APPEND); //记录是否获取到echostr参数
-            $postStr = file_get_contents("php://input");
+          /*  $postStr = file_get_contents("php://input");
            if(!empty($postStr)){
                 $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
                 $fromUsername = $postObj->FromUserName;
@@ -30,8 +30,8 @@
                 $msgType = "text";
                 $contentStr = "Welcome to wechat world!";
                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                    exit($resultStr);               
-           }
+                 exit($resultStr);               
+           }*/
 
                return true;
                       /*
