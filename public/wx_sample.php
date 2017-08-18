1e881ddd46bc9ecf07abda37e79edaab0afdf565
176246469 <176246469@qq.com>
2017-08-18 14:08:35 +0800

    6

--- public/wx_sample.php
+++ public/wx_sample.php
@@ -1,10 +1,11 @@
-<?php   
+<?php
+/*   
         if(strtolower($_SERVER['REQUEST_METHOD']) == 'get') {
             file_put_contents('weixin_log.txt', "IP=".$_SERVER['REMOTE_ADDR'].PHP_EOL,FILE_APPEND); //记录访问IP到log日志
             file_put_contents('weixin_log.txt', "QUERY_STRING=".$_SERVER['QUERY_STRING'].PHP_EOL,FILE_APPEND);//记录请求字符串到log日志
             file_put_contents('weixin_log.txt', '$_GET[echostr])='.htmlspecialchars($_GET['echostr']).PHP_EOL,FILE_APPEND); //记录是否获取到echostr参数
             exit(htmlspecialchars($_GET['echostr']));      //把echostr参数返回给微信开发者后台
-}
+}*/
                 $signature = $_REQUEST['signature'];
                 $timestamp =  $_REQUEST['timestamp'];
                 $nonce =  $_REQUEST['nonce'];
@@ -15,7 +16,28 @@
                 $tmpStr = sha1( $tmpStr );
                 //验证成功
                 if( $tmpStr == $signature ){
-                        return true;
+$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml>
+                                                        <ToUserName><![CDATA[%s]]></ToUserName>
+                                                        <FromUserName><![CDATA[%s]]></FromUserName>
+                                                        <CreateTime>%s</CreateTime>
+                                                        <MsgType><![CDATA[%s]]></MsgType>
+                                                        <Content><![CDATA[%s]]></Content>
+                                                        <FuncFlag>0</FuncFlag>
+                                                        </xml>";             
+                                if(!empty( $keyword ))
+                {
+                        $msgType = "text";
+                        $contentStr = "Welcome to wechat world!";
+                        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                        echo $resultStr;
+                }else{
+                        echo "Input something...";
+                }
                 }
 
 ?>
