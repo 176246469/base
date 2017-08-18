543040dd5c9f649eefd5db0c7d2b04290986da02
176246469 <176246469@qq.com>
2017-08-18 14:03:17 +0800

    5
    5

--- public/wx_sample.php
+++ public/wx_sample.php
@@ -1,4 +1,10 @@
 <?php   
+        if(strtolower($_SERVER['REQUEST_METHOD']) == 'get') {
+            file_put_contents('weixin_log.txt', "IP=".$_SERVER['REMOTE_ADDR'].PHP_EOL,FILE_APPEND); //记录访问IP到log日志
+            file_put_contents('weixin_log.txt', "QUERY_STRING=".$_SERVER['QUERY_STRING'].PHP_EOL,FILE_APPEND);//记录请求字符串到log日志
+            file_put_contents('weixin_log.txt', '$_GET[echostr])='.htmlspecialchars($_GET['echostr']).PHP_EOL,FILE_APPEND); //记录是否获取到echostr参数
+            exit(htmlspecialchars($_GET['echostr']));      //把echostr参数返回给微信开发者后台
+}
                 $signature = $_REQUEST['signature'];
                 $timestamp =  $_REQUEST['timestamp'];
                 $nonce =  $_REQUEST['nonce'];
