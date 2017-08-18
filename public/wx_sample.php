7a4f4e4396ec6ff774c962a1001944f1f7def1b8
176246469 <176246469@qq.com>
2017-08-18 14:22:23 +0800

    555
    55

--- public/wx_sample.php
+++ public/wx_sample.php
@@ -16,28 +16,7 @@
                 $tmpStr = sha1( $tmpStr );
                 //验证成功
                 if( $tmpStr == $signature ){
-$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
-                                                        <ToUserName><![CDATA[%s]]></ToUserName>
-                                                        <FromUserName><![CDATA[%s]]></FromUserName>
-                                                        <CreateTime>%s</CreateTime>
-                                                        <MsgType><![CDATA[%s]]></MsgType>
-                                                        <Content><![CDATA[%s]]></Content>
-                                                        <FuncFlag>0</FuncFlag>
-                                                        </xml>";             
-                                if(!empty( $keyword ))
-                {
-                        $msgType = "text";
-                        $contentStr = "Welcome to wechat world!";
-                        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                        echo $resultStr;
-                }else{
-                        echo "Input something...";
-                }
+                     exit(htmlspecialchars($_GET['echostr'])); 
                 }
 
 ?>
