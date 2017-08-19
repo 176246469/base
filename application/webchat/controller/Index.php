a629f670e382900e135f5fbbb7af0067f60f13f6
176246469 <176246469@qq.com>
2017-08-19 10:31:49 +0800

    整合

--- application/webchat/controller/Index.php
+++ application/webchat/controller/Index.php
@@ -1,29 +1,29 @@
 <?php
-namespace app\weixin\controller;
+namespace app\webchat\controller;
 use think\Request;
 use think\Session;
 use app\admin\model\MouldModel;
 class Index extends   \think\Controller
 {
-
-
-//public  $mould=array('')
+       //首页
         public function main(Request $request){
-
                 //$this->assign('data',$data); 
                 return $this->fetch('main');
         }
+        //文章
         public function article(Request $request){
                 $mould_id=25;
                 $data=MouldModel::getInfo($mould_id,$request->get('id'));
                 $this->assign('data',$data); 
                 return $this->fetch('article');
         }
+        //分类
         public function group(Request $request){
 
                 //$this->assign('data',$data); 
                 return $this->fetch('group');
         }
+        
         public function blind(Request $request){
 
                 //$this->assign('data',$data); 
